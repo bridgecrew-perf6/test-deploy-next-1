1983023d627e035f1546006beaa29fbbe91b450a
salvation27 <nikolayzalipykin@gmail.com>
2022-04-10 18:54:00 +0300

    add polia product API v2.1

--- pages/api/products/productsApi.js
+++ pages/api/products/productsApi.js
@@ -2,9 +2,8 @@ const products = [
   {
     id:1,
     name:'ShopCart-reactClass-Context-Lesson',
-    price:100,
     category:'shop',
-    tehnologies:'reactClass',
+    tehnologies:'react',
     git:'https://github.com/salvation27/react-shop-coditi-provider',
     host:'https://react-shop-codito-provider.vercel.app/',
     source:'https://www.youtube.com/watch?v=FsrwoZsAe9k&t=992s'
